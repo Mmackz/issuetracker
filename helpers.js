a429930c27eab25bfb7207597723c02fa64fdb78
Mmackz <matthewmackenz@gmail.com>
2022-02-23 22:26:02 -0800

    🚧 work in progress

---
+++ helpers.js
@@ -0,0 +1,11 @@
+exports.validParams = [
+   "_id",
+   "issue_text",
+   "issue_text",
+   "created_by",
+   "assigned_to",
+   "status_text",
+   "open",
+   "created_on",
+   "updated_on"
+];
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,44 @@
 "use strict";
+const ObjectID = require("mongodb").ObjectId;
+
+const validParams = require("../helpers").validParams;
 
 module.exports = function (app, db) {
    app.route("/api/issues/:project")
 
       .get(function (req, res) {
          const { project } = req.params;
+         const queries = Object.entries(req.query);
+
+         // remove duplicate params and empty values
+         queries.forEach((q) => {
+            if (Array.isArray(q[1])) {
+               q[1] = [...new Set(q[1])].filter((item) => item !== "");
+               if (q[1].length === 0) {
+                  q[1] = "";
+               }
+               if (q[1].length === 1) {
+                  q[1] = q[1][0];
+               }
+               // if (q[0] === "_id" && ObjectID.isValid(q[1]))
+            }
+         });
+
+         // remove invalid parameters
+         const filteredQueries = Object.fromEntries(
+            queries.filter((q) => validParams.includes(q[0]) && q[1] !== "")
+         );
+
+         // convert _id to db-friendly ObjectID
+         console.log(filteredQueries)
+
+         /* Left off trying to do databse search when multiple ofthe same param are given*/
+         
+                                       // for arrays of values
+         db.find({project, status_text: {$in: ["nope", "ok"]}}).toArray((err, result) => {
+            console.log(result)
+         });
+         res.sendStatus(301);
       })
 
       .post(function (req, res, next) {
@@ -15,21 +49,24 @@ module.exports = function (app, db) {
          if ([issue_text, issue_title, created_by].some((val) => !val)) {
             res.json({ error: "required field(s) missing" });
          } else {
-            const date = new Date()
-            db.insertOne({
-               project,
-               issue_text,
-               issue_text,
-               created_by,
-               assigned_to,
-               status_text,
-               open: true,
-               created_on: date,
-               updated_on: date
-            }, (err, data) => {
-               if (err) console.log(err);
-               console.log(data)
-            });
+            const date = new Date();
+            db.insertOne(
+               {
+                  project,
+                  issue_text,
+                  issue_text,
+                  created_by,
+                  assigned_to,
+                  status_text,
+                  open: true,
+                  created_on: date,
+                  updated_on: date
+               },
+               (err, data) => {
+                  if (err) console.log(err);
+                  console.log(data);
+               }
+            );
          }
       })
 
